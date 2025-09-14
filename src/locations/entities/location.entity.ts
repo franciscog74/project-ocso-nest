b444b531abfd73f4b3f34c2151be00ab4797e1db
franciscog74 <fragrz00@gmail.com>
2025-09-14 17:05:17 -0600

    funcionalidad básica de locations

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,16 @@
-export class Location {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn("increment")
+    locationId: number;
+
+    @Column("text")
+    locationName: string;
+
+    @Column("text")
+    locationAddress: string;
+
+    @Column("array")
+    locationLatLng: number[];
+}
--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationsService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+  constructor(
+    private locationRepository: Repository<Location>
+  ) {}
+
+  async create(createLocationDto: CreateLocationDto) {
+    return await this.locationRepository.save(createLocationDto);
   }
 
-  findAll() {
-    return `This action returns all locations`;
+  async findAll() {
+    return await this.locationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} location`;
+  async findOne(id: number) {
+    const location = await this.locationRepository.findOneBy({
+      locationId: id
+    });
+    if (!location)
+      throw new NotFoundException();
+    return location;
   }
 
-  update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
+  async update(id: number, updateLocationDto: UpdateLocationDto) {
+    const locationToUpdate = await this.locationRepository.preload({
+      locationId: id,
+      ...updateLocationDto
+    });
+    if (!locationToUpdate)
+      throw new NotFoundException();
+    await this.locationRepository.save(locationToUpdate);
+    return locationToUpdate;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} location`;
+  async remove(id: number) {
+    const del = await this.locationRepository.delete({
+      locationId: id
+    });
+    if (!del.affected)
+      throw new NotFoundException();
+    return {
+      message: `Objeto con id ${id} eliminado`
+    };
   }
 }
